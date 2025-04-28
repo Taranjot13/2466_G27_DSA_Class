5c0bba96ca018aa9d9fc585bfc87456386889db3
Taranjot Singh <140800202+Taranjot13@users.noreply.github.com>
2025-04-28 12:33:27 +0530

    Update BinaryTreesYT.java

--- BinaryTreesYT.java
+++ BinaryTreesYT.java
@@ -1,104 +1,167 @@
 import java.util.*;
 
 public class BinaryTreesYT {
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
     }
-    static class BinaryTree{
-        static int idx=-1;
-        public static Node buildTree(int nodes[]){
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
             idx++;
-            if(nodes[idx]==-1){
+            if (nodes[idx] == -1) {
                 return null;
             }
 
-            Node newNode=new Node(nodes[idx]);
-            newNode.left=buildTree(nodes);
-            newNode.right=buildTree(nodes);
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
 
             return newNode;
         }
     }
-    public static void preorder(Node root){
-        if(root==null){
+
+    public static void preorder(Node root) {
+        if (root == null) {
             return;
         }
-        System.out.print(root.data);
+        System.out.print(root.data + " ");
         preorder(root.left);
         preorder(root.right);
     }
-    public static void inorder(Node root){
-        if(root==null){
+
+    public static void inorder(Node root) {
+        if (root == null) {
             return;
         }
         inorder(root.left);
-        System.out.print(root.data);
+        System.out.print(root.data + " ");
         inorder(root.right);
     }
-    public static void postorder(Node root){
-        if(root==null){
+
+    public static void postorder(Node root) {
+        if (root == null) {
             return;
         }
         postorder(root.left);
         postorder(root.right);
-        System.out.print(root.data);
+        System.out.print(root.data + " ");
     }
+
     public static void levelOrder(Node root) {
         System.out.println();
         if (root == null) {
             return;
         }
-        Queue <Node> q=new LinkedList<>();
+        Queue<Node> q = new LinkedList<>();
         q.add(root);
         q.add(null);
-        while(!q.isEmpty()){
-            Node currNode=q.remove();
-            if(currNode==null){
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
                 System.out.println();
-                if(q.isEmpty()){
+                if (q.isEmpty()) {
                     break;
-                }
-                else {
+                } else {
                     q.add(null);
                 }
-            }else {
-                System.out.print(currNode.data+" ");
-                if(currNode.left!=null){
+            } else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
                     q.add(currNode.left);
                 }
-                if(currNode.right!=null){
+                if (currNode.right != null) {
                     q.add(currNode.right);
                 }
             }
         }
     }
 
+    public static int countOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftNodes = countOfNodes(root.left);
+        int rightNodes = countOfNodes(root.right);
+
+        return leftNodes + rightNodes + 1;
+    }
+
+    public static int sumOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+
+        return leftSum + rightSum + root.data;
+    }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lefth = height(root.left);
+        int righth = height(root.right);
+        int myh = Math.max(lefth, righth) + 1;
+        return myh;
+    }
+
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int diam1 = diameter(root.left);
+        int diam2 = diameter(root.right);
+        int diam3 = height(root.left) + height(root.right) + 1;
+
+        int diah = Math.max(diam3, Math.max(diam1, diam2));
+        return diah;
+    }
 
     public static void main(String[] args) {
-        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        BinaryTree tree=new BinaryTree();
-        Node root=tree.buildTree(nodes);
-        System.out.print("root:");
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+
+        System.out.print("root: ");
         System.out.println(root.data);
-        System.out.print("preorder:");
+
+        System.out.print("preorder: ");
         preorder(root);
         System.out.println();
-        System.out.print("inorder:");
+
+        System.out.print("inorder: ");
         inorder(root);
         System.out.println();
-        System.out.print("postorder:");
+
+        System.out.print("postorder: ");
         postorder(root);
         System.out.println();
+
         System.out.print("Level order:");
         levelOrder(root);
-        System.out.println();
+
+        System.out.print("count of nodes: ");
+        System.out.println(countOfNodes(root));
+
+        System.out.print("sum of nodes: ");
+        System.out.println(sumOfNodes(root));
+
+        System.out.print("height of nodes: ");
+        System.out.println(height(root));
+
+        System.out.print("diameter of nodes: ");
+        System.out.println(diameter(root));
     }
 }
